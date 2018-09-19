870353fcfff561e90caeb8f19b682975c6e02a4f
Xmader <xmader@outlook.com>
2018-09-19 15:38:54 +0800

    删除旧版的文件

--- index.js
+++
@@ -1,8 +0,0 @@
-const pics_iterator = pics[Symbol.iterator]();
-
-const main = () => {
-    var pic = pics_iterator.next().value
-    pic ? (document.getElementById("pics").innerText = pic) : clearInterval(handle)
-}
-
-const handle = setInterval(main, 100)
