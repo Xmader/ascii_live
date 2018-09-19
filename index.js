a3ba2aad3eb5bf39e2b8b52db7b20713c6dc30aa
Xmader <xmader@outlook.com>
2018-09-19 16:04:32 +0800

    v1.0

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+/*!
+ * ASCII在线视频流 (https://www.xmader.com/ascii_live/)
+ * 
+ * Copyright (c) 2018 Xmader
+ * Released under the MIT license
+ * 
+ * Source Code: https://github.com/Xmader/ascii_live
+ * 
+ * 为了更好的兼容性，不使用任何ES6特性
+ * 
+*/
+
 var video = document.getElementById("video1");
 var cv = document.getElementById('cv');
 var ctx = cv.getContext('2d');
 var txtDiv = document.getElementById('txt');
 var uploadBtn = document.getElementById("upload-button");
 
+var txt_style = document.getElementById("txt").style
+var font_size_range = document.getElementById("font_size_range")
+var font_size_span = document.getElementById("font_size")
+
 var font_size = 2 // ASCII字符画的字号，数值越小分辨率越高，占用的CPU百分比也就越高 (和占用的内存没有关系)，必须是2的倍数，单位: px (像素)
 
 // 根据调整字号滑动条动态地改变ASCII字符画的字号
 function chang_font_size() {
-    var txt_style = document.getElementById("txt").style
-
-    var n = +document.getElementById("font_size_range").value
-    document.getElementById("font_size").innerText = n
+    var n = +font_size_range.value // 获取的原始值是一个字符串，用"+"号将它转换成一个数字
+    font_size_span.innerText = n
 
     txt_style["font-size"] = txt_style["line-height"] = n + "px" // 设置ASCII字符画显示区域的字号和行高为设置的数值
 
